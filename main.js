cca2110929f54ccbb61ee7cccf41893a2cd30b96
Rita <ritamitsuhashi@gmail.com>
2023-08-02 15:07:28 -0300

    edited code for image changing on click

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
-function verGif()
-{
-    alert("clicou");
+/* let imgSofa = document.getElementById("sofaEstatico"); */
+
+function verGif() {
+  if (
+    document.getElementById("sofaEstatico").src == "./assets/card-sofa1.png"
+  ) {
+    document.getElementById("sofaEstatico").src =
+      "./assets/Sofá #boraCodar.gif";
+  } else {
+    document.getElementById("sofaEstatico").src = "./assets/card-sofa1.png";
+  }
 }
 
-function adicionarProduto()
-{
-    alert("Produto adicionado à cesta.");
+function adicionarProduto() {
+  alert("Produto adicionado à cesta.");
 }
