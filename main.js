6e6c76b008d9dbd134e736dca7166dc80cdfaf73
Rita <ritamitsuhashi@gmail.com>
2023-08-03 15:11:17 -0300

    testing onclick

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 /* O código abaixo só troca uma vez. Outros problemas: tamanho do gif / trocar o .svg do 360 pra um "x" de fechar animação.
+
 let sofaEstatico = document.querySelector('#sofaEstatico');
 
 function verGif() {
   sofaEstatico.src = './assets/card-sofa-girando.gif';
 }
 
-*/
 
 function verGif() {
   if (
@@ -16,6 +16,21 @@ function verGif() {
     document.getElementById("sofaEstatico").src = "./assets/card-sofa-estatico.png";
   }
 }
+*/
+
+let sofaEstatico = document.getElementById('#sofaEstatico');
+
+function verGif() {
+  if ( sofaEstatico.src == '/assets/card-sofa-estatico.png'
+  ) {
+  sofaEstatico.src = '/assets/card-sofa-girando.gif';
+  } else {
+    sofaEstatico.src = '/assets/card-sofa-estatico.png';
+  }
+}
+
+
+
 
 function adicionarProduto() {
   alert("Produto adicionado à cesta.");
